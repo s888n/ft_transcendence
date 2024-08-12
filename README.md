4af0721bfbf451c114308e3fc8bec02e040c21a2
yahyatoumi <yahyatoumi555@gmail.com>
2024-08-12 16:58:06 +0100

    clean up

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 ## todos:
 
 ## To run :
-    - Change `10.13.11.1` with your ip in both  `.env` and `frontend/.env.production` files
+    - Change `10.13.3.12` with your ip in both  `.env` and `frontend/.env.production` files
     - run `docker compose up`
 
 ## Modules
--- frontend/next.config.js
+++ frontend/next.config.js
@@ -29,9 +29,8 @@ const nextConfig = {
         source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: NEXT_PUBLIC_SERVER_ENDPOINT }, // Replace with your frontend origin
+          { key: "Access-Control-Allow-Origin", value: NEXT_PUBLIC_SERVER_ENDPOINT },
           { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT,OPTIONS" },
-          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
         ]
       }
     ]
